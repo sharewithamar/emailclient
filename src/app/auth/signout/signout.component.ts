8787da5de5822c1cdb223f9f58397a4b507e682e
Amarnath <sharewithamar@gmail.com>
2020-04-15 15:34:59 +0530

    signout navigation - Router.NavigateByUrl

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signout',
@@ -7,11 +8,12 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./signout.component.css'],
 })
 export class SignoutComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.authService.signout().subscribe(() => {
       //Navigate the user back to a signin page
+      this.router.navigateByUrl('/');
     });
   }
 }
